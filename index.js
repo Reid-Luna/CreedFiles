7c9cb2f36c1b40a24835d379e092d962fa5439d4
Reid Luna <reidluna@icloud.com>
2018-11-27 17:01:01 -0700

    SSL Support

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
+const key = fs.readFileSync(`${__dirname}/ssl/creedfiles_com.key`);
+const cert = fs.readFileSync(`${__dirname}/ssl/creedfiles_com.crt`);
+const ca = fs.readFileSync(`${__dirname}/ssl/creedfiles_com.ca-bundle`);
+
+const credentials = {
+  key,
+  cert,
+  ca
+};
+
 const Seasons = require("./SeasonSchema");
 
 const init = () => {
   app.use(express.static(`${__dirname}/frontend/build`));
-
-  app.get("/.well-known/pki-validation/", (req, res) => {
-    res.sendFile(`${__dirname}/DA7AD91CFBC3405C0AE35CC8CBC6EEB6.txt`);
-  });
 
   const getLimit = async season => {
     season = season ? { season } : {};
@@ -101,8 +111,16 @@ const init = () => {
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
-  app.listen(80, () => {
-    console.log("app running on 80");
+
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
+
+  httpServer.listen(80, () => {
+    console.log("HTTP Server Running on Port 80");
+  });
+
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server Running on Port 443");
   });
 };
 
