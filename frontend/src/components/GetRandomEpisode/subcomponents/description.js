c0a8cbd512ee5234c08dbf29a0aa5c7d18939e63
reid luna <reidluna@icloud.com>
2018-12-02 20:43:06 -0700

    Cleaned up

--- frontend/src/components/GetRandomEpisode/subcomponents/description.js
+++ frontend/src/components/GetRandomEpisode/subcomponents/description.js
@@ -20,7 +20,10 @@ class Description extends Component {
       return (
         <p>
           {this.state.description}
-          <a style={{ color: "#fff" }}>{"#".repeat(100 - this.state.length)}</a>
+          <a style={{ color: "#fff" }}>
+            {" "}
+            {"#".repeat(100 - this.state.length)}
+          </a>
         </p>
       );
     } else if (!this.state.expand && this.state.length > 100) {
